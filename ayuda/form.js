60b2109902a16c20752a7c7cf6e25ee3dd99dc54
raultamos <56404030+raultamos@users.noreply.github.com>
2026-03-02 23:12:34 -0400

    completely revamped helppage, added formspree API, and fixed animations in the nav

--- ayuda/form.js
+++ ayuda/form.js
@@ -1,13 +1,7 @@
-window.onload = function() {
-    document.getElementById("contactForm").style.display = "block"; // Asegúrate de que esté oculto en CSS inicialmente
-};
-const close = document.getElementById("close");
-const open = document.getElementById("open");
-const modal = document.getElementById("modal");
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-open.addEventListener("click", () => modal.classList.add("show-modal"));
-close.addEventListener("click", () => modal.classList.remove("show-modal"));
-
-window.addEventListener("click", (e) => {
-  e.target === modal ? modal.classList.remove("show-modal") : false;
-});
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
